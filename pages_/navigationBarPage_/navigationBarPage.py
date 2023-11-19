7a600f018c14c27862fb8442d00bb7ce4d9d1f32
Jenya Kirakosyan <jenyakirakosyan27@gmail.com>
2023-11-20 01:17:31 +0400

    Amazon tests Envirnment common, peges, tests

---
+++ pages_/navigationBarPage_/navigationBarPage.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from pages_.basePage import BasePage
+
+
+class NavigationBar(BasePage):
+    def __init__(self, driver: webdriver.Chrome):
+        super().__init__(driver)
+        self.__cartButtonLocator = (By.ID, "nav-cart-count-container")
+        self.__productNameFieldLocator = (By.ID, "twotabsearchtextbox")
+        self.__searchButtonLocator = (By.ID, "nav-search-submit-button")
+
+
+    def click_to_cart_button(self):
+        cartButtonElement = self._find_element(self.__cartButtonLocator)
+        self._click_to_element(cartButtonElement)
+
+    def fill_search_field(self, searchElement):
+        productNameFieldElement = self._find_element(self.__productNameFieldLocator)
+        self._fill_field(productNameFieldElement, searchElement)
+
+    def click_to_search_button(self):
+        searchButtonElement = self._find_element(self.__searchButtonLocator)
+        self._click_to_element(searchButtonElement)
---
+++ tests_/loginTests_/loginTest.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+from pages_.loginPage_.loginPage import LogInPage
+from time import sleep
+from selenium.webdriver.support.events import EventFiringWebDriver
+from common_.utilities_.customListener import MyListener
+
+
+class LogInTest(unittest.TestCase):
+    def setUp(self):
+        self.simpleDriver = webdriver.Chrome()
+        self.driver = EventFiringWebDriver(self.simpleDriver, MyListener())
+        self.driver.implicitly_wait(10)
+        self.driver.delete_all_cookies()
+        self.driver.maximize_window()
+        self.driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fsign%2Fs%3Fk%3Dsign%2Bin%26ref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+
+    def test_logIn(self):
+        # test Amazon Log in with valid data
+        logInPageObj = LogInPage(self.driver)
+        logInPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
+        logInPageObj.click_to_continue_button()
+        logInPageObj.fill_password_fild("//eva[@ts]")
+        sleep(5)  # Added sleep time to avoid captcha from amazon
+        logInPageObj.click_to_signin_button()
+        self.assertEqual(self.driver.title, "Amazon Sign-In")
+
+    def test_logIn_with_invalid_password(self):
+        logInPageObj = LogInPage(self.driver)
+        logInPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
+        logInPageObj.click_to_continue_button()
+        logInPageObj.fill_password_fild("WrongPassword")
+        sleep(5)  # Added sleep time to avoid captcha from amazon
+        logInPageObj.click_to_signin_button()
+        logInPageObj.validate_incorrect_password_alert()
+
+    def test_logIn_with_invalid_email_address_or_mobile_phone_number(self):
+        logInPageObj = LogInPage(self.driver)
+        logInPageObj.fill_username_field("jenyakirakosyan2@gmail.com")
+        logInPageObj.click_to_continue_button()
+        logInPageObj.validate_incorrect_email_address_or_mobile_phone_number_alert()
+
+    def tearDown(self):
+        self.driver.close()
+
+
